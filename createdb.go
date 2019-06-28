2959a6053b1406176958c96d152f1fa7172b7e10
Kevin Parrish <Kevin.Parrish@vsp.com>
2019-06-28 14:18:25 -0700

    Begining mods to test hash of routerID and List ID.

--- createdb.go
+++ createdb.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+/*
 func createdb() {
 	database, _ := sql.Open("sqlite3", "./nraboy.db")
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
@@ -21,5 +22,22 @@ func createdb() {
 	for rows.Next() {
 		rows.Scan(&id, &firstname, &lastname)
 		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
+}
+*/
+
+func main() {
+	database, _ := sql.Open("sqlite3", "./hashed.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, router1 TEXT, router2 TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO people (router1, router2) VALUES (?, ?)")
+	statement.Exec("media", "router")
+	rows, _ := database.Query("SELECT id, router1, router2 FROM people")
+	var id int
+	var router1 string
+	var router2 string
+	for rows.Next() {
+		rows.Scan(&id, &router1, &router2)
+		fmt.Println(strconv.Itoa(id) + ": " + router1 + " " + router2)
 	}
 }
