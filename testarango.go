f84b9d21f390cc54633e4627f3e76518c5995201
Kevin Parrish <kparrish@surewest.net>
2019-08-04 17:00:05 -0700

    commented out document creation code. Moved testarango.go to root govisn directory to facilitate compilation.

---
+++ testarango.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	driver "github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver/http"
+)
+
+func testarango() {
+
+	conn, err := http.NewConnection(http.ConnectionConfig{
+		Endpoints: []string{"http://server:8529"},
+	})
+	if err != nil {
+		// Handle error
+	}
+	client, err := driver.NewClient(driver.ClientConfig{
+		Connection: conn,
+	})
+	if err != nil {
+		// Handle error
+	}
+
+	// Open "examples_books" database
+	db, err := client.Database(nil, "examples_books")
+	if err != nil {
+		// Handle error
+	}
+
+	// Open "books" collection
+	col, err := db.Collection(nil, "books")
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println("col=", col)
+	/*
+		// Create document
+		book := Book{
+			Title:   "ArangoDB Cookbook",
+			NoPages: 257,
+		}
+		meta, err := col.CreateDocument(nil, book)
+		if err != nil {
+			// Handle error
+		}
+		fmt.Printf("Created document in collection '%s' in database '%s'\n", col.Name(), db.Name())
+	*/
+}
