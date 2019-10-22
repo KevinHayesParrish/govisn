f7bc6c25f218d631f4d079e0cffe6c6a5c64915f
Kevin Parrish <kevipa@lmac1b9wlvcg.vsp.com>
2019-10-22 11:58:45 -0700

    Changed Earth globe opacity to 0.25.

--- viewnet.go
+++ viewnet.go
@@ -200,7 +200,7 @@ func main() {
 	globe3D := geometry.NewSphere(globeRadius, 16, 16, 0, math.Pi*2, 0, math.Pi)
 	globeMat := material.NewPhong(&math32.Color{R: 0.5, G: 0.5, B: 0.5})
 	globeMat.SetTransparent(true)
-	globeMat.SetOpacity(0.5)
+	globeMat.SetOpacity(0.25)
 	globeMesh := graphic.NewMesh(globe3D, globeMat)
 	globeMesh.SetPosition(0, 0, 0)
 	app.Scene().Add(globeMesh)
