ab3949d6d471fc4baa9705f7b69763b4ee1c55fb
Kevin Parrish <kevin@kevin-mac.parrish.home>
2020-04-14 19:24:30 -0700

    Added .o to interfaces, ipAddrTable and ipRouteTable OIDS.

--- discover.go
+++ discover.go
@@ -75,13 +75,13 @@ func discover(debugFlag bool, snmpTarget string, community string, maxHopsStr st
 
 	// Retrive sysName, sysDescr, sysContact, sysLocation
 	oids := []string{
-		"1.3.6.1.2.1.1.5.0", // sysName
-		"1.3.6.1.2.1.1.1.0", // sysDescr
-		"1.3.6.1.2.1.1.4.0", // sysContact
-		"1.3.6.1.2.1.1.6.0", // sysLocation
-		"1.3.6.1.2.1.2",     // interfaces
-		"1.3.6.1.2.1.4.20",  // ipAddrTable
-		"1.3.6.1.2.1.4.21",  // ipRouteTable
+		"1.3.6.1.2.1.1.5.0",  // sysName
+		"1.3.6.1.2.1.1.1.0",  // sysDescr
+		"1.3.6.1.2.1.1.4.0",  // sysContact
+		"1.3.6.1.2.1.1.6.0",  // sysLocation
+		"1.3.6.1.2.1.2.0",    // interfaces
+		"1.3.6.1.2.1.4.20.0", // ipAddrTable
+		"1.3.6.1.2.1.4.21.0", // ipRouteTable
 	}
 	result, err2 := params.Get(oids) // Get() accepts up to g.MAX_OIDS
 	if err2 != nil {
