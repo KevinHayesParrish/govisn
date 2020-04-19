9bab293f81082d231c957f31eda79f84bbcaebb0
Kevin Parrish <kparrish@surewest.net>
2020-04-18 22:05:45 -0700

    Initial Commit.

---
+++ MIB.go
@@ -0,0 +1,15 @@
+package main
+
+// MIB
+const systemOID string = "1.3.6.1.2.1.1"
+const sysDescrOID string = "1.3.6.1.2.1.1.1"
+const sysUpTimeOID string = "1.3.6.1.2.1.1.3"
+const sysContactOID string = "1.3.6.1.2.1.1.4"
+const sysNameOID string = "1.3.6.1.2.1.1.5"
+const sysLocationOID string = "1.3.6.1.2.1.1.6"
+const sysServicesOID string = "1.3.6.1.2.1.1.7"
+const interfacesOID string = "1.3.6.1.2.1.2"
+const ifNumberOID string = "1.3.6.1.2.1.2.1"
+const ifTableOID string = "1.3.6.1.2.1.2.2"
+const ipAddrTableOID string = "1.3.6.1.2.1.4.20"
+const ipRouteTableOID string = "1.3.6.1.2.1.4.21"
