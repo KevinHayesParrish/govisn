410ce44befbbab0da7454c74f710591d72eeac75
Kevin Parrish <kparrish@surewest.net>
2020-05-14 20:20:02 -0700

    Listed TODO items.

--- buildLinks.go
+++ buildLinks.go
@@ -15,6 +15,14 @@ const buildLinksVersion = "0.0.1"
 func buildLinks(debugFlag bool, database *sql.DB) *sql.DB {
 	fmt.Println("func buildLinks version", buildLinksVersion, "started")
 
+	/* TODO
+	 *
+	 * Populate RouterName, DestinationName, DestinationIP, NextHopName and NextHopIP from RouteTable elements.
+	 * calculate LinkID using CRC of RouterName/DestinationIP/NextHopIP
+	 * Write Links Row to database
+	 *
+	 */
+
 	fmt.Println("func buildLinks version", buildLinksVersion, "stopped")
 	return database
 }
