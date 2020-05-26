7ec23efaf42603fd3062b4a5b264ddcd6ca52cda
Kevin Parrish <kparrish@surewest.net>
2020-05-25 19:49:46 -0700

    Corrected mismatch of select columns between LinkTableRows Query and Scan.

--- getRouterCoordinatesIP.go
+++ getRouterCoordinatesIP.go
@@ -13,7 +13,7 @@ func getRouterCoordinatesIP(debugFlag bool, database *sql.DB, ToRouterIPIn strin
 	var y float32
 	var z float32
 
-	LinksTableRows, err := database.Query("SELECT RouterID FROM RouterIP WHERE IpAddr = $1", ToRouterIPIn)
+	LinksTableRows, err := database.Query("SELECT RouterID  FROM RouterIP WHERE IpAddr = $1", ToRouterIPIn)
 	if err != nil {
 		log.Fatalln("LinksTableRows error", err.Error())
 	}
@@ -22,10 +22,10 @@ func getRouterCoordinatesIP(debugFlag bool, database *sql.DB, ToRouterIPIn strin
 	}
 	defer LinksTableRows.Close()
 
-	var RouterIDLinks, IPAddrLinks string
+	var RouterIDLinks string
 	var RouterIDRouter, GpsLat, GpsLong, GpsAlt string
 	for LinksTableRows.Next() {
-		LinksTableRows.Scan(&RouterIDLinks, &IPAddrLinks)
+		LinksTableRows.Scan(&RouterIDLinks)
 
 		RouterTableRows, err := database.Query("SELECT RouterID, GpsLat, GpsLong, GpsAlt FROM Routers WHERE RouterID = $1", RouterIDLinks)
 		if err != nil {
