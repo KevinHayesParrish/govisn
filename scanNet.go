d2035fd5c34ac19c4b72cfed34765cf37586a1f5
Kevin Parrish <kparrish@surewest.net>
2020-05-31 15:39:32 -0700

    Removed database as a called Parameter and added params as a called Parameter.

--- scanNet.go
+++ scanNet.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
+	g "github.com/soniah/gosnmp"
 )
 
 /*
@@ -15,7 +15,7 @@ import (
 // SCANNETVERSION is the file version number
 const SCANNETVERSION = "0.0.1"
 
-func scanNet(debugFlag bool, seed string, community string, database *sql.DB) []ScannedRouter {
+func scanNet(debugFlag bool, seed string, community string, params g.GoSNMP) []ScannedRouter {
 
 	fmt.Println("\nfunc scanNet version", SCANNETVERSION, "started.")
 	if debugFlag {
