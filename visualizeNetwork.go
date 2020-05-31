54731535994d3e4b739d34c76ca08c38da2b7b16
Kevin Parrish <kparrish@surewest.net>
2020-05-31 13:57:56 -0700

    Removed Commented lines.

--- visualizeNetwork.go
+++ visualizeNetwork.go
@@ -8,8 +8,6 @@ import (
 	"os"
 	"strconv"
 
-	//	"github.com/g3n/g3nd/material"
-
 	"github.com/g3n/engine/geometry"
 	"github.com/g3n/engine/gls"
 	"github.com/g3n/engine/graphic"
@@ -30,28 +28,21 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	}
 
 	// Retrieve the Routers table
-	//	routerRows, queryErr := databaseForRead.Query("SELECT RouterID, SystemName, SystemDesc, UpTime, Contact, Location, GpsLat, GpsLong, GpsAlt FROM Routers")
-	//	routerRows, queryErr := databaseForRead.Query("SELECT RouterID, Name, Description, UpTime, Contact, Location, GpsLat, GpsLong, GpsAlt FROM Routers")
 	routerRows, queryErr := databaseForRead.Query("SELECT RouterID, Name, Description, UpTime, Contact, Location, Services, GpsLat, GpsLong, GpsAlt FROM Routers")
 	if queryErr != nil {
 		fmt.Println("databaseForRead Query error", queryErr)
 		log.Fatal(queryErr)
 	}
-	//	if *debugFlag {
 	if debugFlag {
 		fmt.Println("Successful Routers table Select")
 	}
 
 	// Retrieve the Links table
-	//	linkRows, queryErr := databaseForRead.Query("SELECT LinkID, FromRouter, ToRouter FROM Links")
-	//	linkRows, queryErr := databaseForRead.Query("SELECT LinkID, FromRouterName, FromRouterIP, ToRouterName, FromRouterIP FROM Links")
-	//	linkRows, queryErr := databaseForRead.Query("SELECT LinkID, RouterName, DestinationName, DestinationIP, NextHopName, NextHopIP FROM Links")
 	linkRows, queryErr := databaseForRead.Query("SELECT LinkID, FromRouterName, FromRouterIP, ToRouterName, ToRouterIP FROM Links")
 	if queryErr != nil {
 		fmt.Println("databaseForRead Query error", queryErr)
 		log.Fatal(queryErr)
 	}
-	//	if *debugFlag {
 	if debugFlag {
 		fmt.Println("Successful Links table Select")
 	}
@@ -70,7 +61,6 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	var RouterID int
 	var Name string
 	var Description string
-	//	var UpTime string
 	var UpTime uint32
 	var Contact string
 	var Location string
@@ -78,21 +68,8 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	var GpsLat string
 	var GpsLong string
 	var GpsAlt string
-	//	var X3D float32
-	//	var Y3D float32
-	//	var Z3D float32
-	//	var router Router
-	//	var routerArray [1000]Router
-
-	//	var routerArray [maxRouters]Router
-
 	var link Link
 	var LinkID int
-	//	var FromRouterName string
-	//	var FromRouterIP string
-	//	var ToRouterName string
-	//	var ToRouterIP string
-	//	var RouterName, DestinationName, DestinationIP, NextHopName, NextHopIP string
 	var FromRouterName, FromRouterIP, ToRouterName, ToRouterIP string
 	var x float32
 	var y float32 = 1.0
@@ -102,10 +79,9 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	ambientLight := light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.8)
 	app.Scene().Add(ambientLight)
 	pointLight := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 5.0)
-	//	pointLight.SetPosition(1, 0, 2)
-	//	pointLight.SetPosition(10, 10, 20)
 	pointLight.SetPosition((float32)(globeRadius+10), (float32)(globeRadius+10), (float32)(globeRadius+20))
 	app.Scene().Add(pointLight)
+
 	// Add an axis helper to the scene
 	axis := graphic.NewAxisHelper(0.5)
 	app.Scene().Add(axis)
@@ -115,7 +91,6 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 
 	// Create a sphere representing the globe
 	globe3D := geometry.NewSphere(globeRadius, 16, 16, 0, math.Pi*2, 0, math.Pi)
-	//	globeMat := material.NewPhong(&math32.Color{R: 0.5, G: 0.5, B: 0.5})
 	globeMat := material.NewPhong(&math32.Color{R: 0.0, G: 0.5, B: 1.0}) // Azure blue 0, 128, 255
 	globeMat.SetTransparent(true)
 	globeMat.SetOpacity(0.25)
@@ -123,7 +98,6 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	globeMesh.SetPosition(0, 0, 0)
 	app.Scene().Add(globeMesh)
 
-	//	if *debugFlag {
 	if debugFlag {
 		fmt.Println("Beginning routerRows.Next loop; adding routers to 3D scene.")
 	}
@@ -134,20 +108,9 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	var router Router
 	routerArrayIndex := 0
 	for routerRows.Next() {
-		//		routerRows.Scan(&RouterID, &Name, &Description, &UpTime, &Contact, &Location, &GpsLat, &GpsLong, &GpsAlt)
 		routerRows.Scan(&RouterID, &Name, &Description, &UpTime, &Contact, &Location, &Services, &GpsLat, &GpsLong, &GpsAlt)
 
 		// Load router struct from DB fields
-		//		routerArray[routerArrayIndex].System.RouterID = RouterID
-		//		routerArray[routerArrayIndex].System.UpTime = UpTime
-		//		routerArray[routerArrayIndex].System.Name = Name
-		//		routerArray[routerArrayIndex].System.Contact = Contact
-		//		routerArray[routerArrayIndex].System.Location = Location
-		//		routerArray[routerArrayIndex].System.Services = Services
-		//		routerArray[routerArrayIndex].System.GPS.Latitude = GpsLat
-		//		routerArray[routerArrayIndex].System.GPS.Longitude = GpsLong
-		//		routerArray[routerArrayIndex].System.GPS.Altitude = GpsAlt
-
 		router.System.RouterID = RouterID
 		router.System.UpTime = UpTime
 		router.System.Name = Name
@@ -168,31 +131,19 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 		 */
 		x, y, z = calcCoordinates(GpsLat, GpsLong, GpsAlt)
 
-		//		routers[routerArrayIndex].System.GPS.Longitude = fmt.Sprintf("%f", x) // update router struc with x coordinate
-
-		//		routers[routerArrayIndex].System.GPS.Latitude = fmt.Sprintf("%f", y) // update router struc with y coordinate
-
-		//		routers[routerArrayIndex].System.GPS.Altitude = fmt.Sprintf("%f", z) // update router struc with z coordinate.
-
 		if debugFlag {
 			fmt.Println("x =", x, "y =", y, "z", z)
-			//			fmt.Println("router =", routerArray[routerArrayIndex])
 			fmt.Println("router =", routers[routerArrayIndex])
-			//			fmt.Println("router.System.Coordinates =", routerArray[routerArrayIndex].System.Coordinates)
-			//			fmt.Println("router.System.GPS =", routerArray[routerArrayIndex].System.GPS)
 			fmt.Println("router.System.GPS =", routers[routerArrayIndex].System.GPS)
 			fmt.Println("RouterID=", RouterID, "Name=", Name)
 		}
 
 		// Add Router object to 3D scene.
-		//		cylinderMesh.SetPosition(routerArray[routerArrayIndex].System.Coordinates.X, routerArray[routerArrayIndex].System.Coordinates.Y, routerArray[routerArrayIndex].System.Coordinates.Z)
-		//		cylinderMesh.SetPosition(routerArray[routerArrayIndex].System.GPS.Longitude, routerArray[routerArrayIndex].System.GPS.Latitude, routerArray[routerArrayIndex].System.GPS.Altitude)
 		cylinderMesh.SetPosition(x, y, z)
 		app.Scene().Add(cylinderMesh)
 
 		// Add router name to scene
 		// Creates Font
-		//		fontfile := app.DirData() + "/fonts/FreeSans.ttf"
 		fontfile := os.Getenv("GOPATH") + "/src/govisn/data/fonts/FreeSans.ttf"
 		font, err := text.NewFont(fontfile)
 		if err != nil {
@@ -205,7 +156,6 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 		font.SetFgColor(&math32.Color4{R: 0, G: 0, B: 1, A: 1})
 		font.SetBgColor(&math32.Color4{R: 1, G: 1, B: 0, A: 0.8})
 		canvas := text.NewCanvas(300, 200, &math32.Color4{R: 0, G: 1, B: 0, A: 0.8})
-		//		rtext := "RouterID: " + strconv.Itoa(routerArray[routerArrayIndex].System.RouterID) + "\nHostname: " + routerArray[routerArrayIndex].System.Name
 		rtext := "RouterID: " + strconv.Itoa(routers[routerArrayIndex].System.RouterID) + "\nHostname: " + routers[routerArrayIndex].System.Name
 		swidth, sheight := font.MeasureText(rtext)
 		canvas = text.NewCanvas(swidth, sheight, &math32.Color4{R: 0, G: 1, B: 1, A: 1})
@@ -215,7 +165,6 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 		mat3.AddTexture(tex3)
 		aspect := float32(swidth) / float32(sheight)
 		mesh3 := graphic.NewSprite(aspect, 1, mat3)
-		//		mesh3.SetPosition(routerArray[routerArrayIndex].System.Coordinates.X, routerArray[routerArrayIndex].System.Coordinates.Y, routerArray[routerArrayIndex].System.Coordinates.Z+1.0)
 		mesh3.SetPosition(x, y, z+1.0)
 		app.Scene().Add(mesh3)
 
@@ -228,7 +177,6 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	}
 	defer routerRows.Close()
 
-	//	if *debugFlag {
 	if debugFlag {
 		fmt.Println("\nBeginning linkRows.Next loop; adding links to the 3D scene")
 		fmt.Println()
@@ -236,12 +184,8 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 	/*
 	* Add the links to the 3D scene
 	 */
-	//	var FromRouterX, FromRouterY, FromRouterZ, ToRouterX, ToRouterY, ToRouterZ float32
 	var FromRouterX, FromRouterY, FromRouterZ, ToRouterX, ToRouterY, ToRouterZ string
 	for linkRows.Next() {
-		//		err := linkRows.Scan(&LinkID, &FromRouter, &ToRouter)
-		//		err := linkRows.Scan(&LinkID, &FromRouterName, &FromRouterIP, &ToRouterName, &ToRouterIP)
-		//		err := linkRows.Scan(&LinkID, &RouterName, &DestinationName, &DestinationIP, &NextHopName, &NextHopIP)
 		err := linkRows.Scan(&LinkID, &FromRouterName, &FromRouterIP, &ToRouterName, &ToRouterIP)
 		if err != nil {
 			log.Fatal(err)
@@ -277,19 +221,15 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 		}
 		defer routerGpsRows.Close()
 		var linksFromRouterName string
-		//		var linksFromRouterGpsLat, linksFromRouterGpsLong, linksFromRouterGpsAlt float32
 		var linksFromRouterGpsLat, linksFromRouterGpsLong, linksFromRouterGpsAlt string
 		for routerGpsRows.Next() {
 			routerGpsRows.Scan(&linksFromRouterName, &linksFromRouterGpsLat, &linksFromRouterGpsLong, &linksFromRouterGpsAlt)
 		}
 
-		//		FromRouterX, FromRouterY, FromRouterZ = getRouterCoordinatesName(debugFlag, routers, link.FromRouterName)
-		//		FromRouterX, FromRouterY, FromRouterZ = getRouterCoordinatesName(debugFlag, routers, link.FromRouterName)
 		FromRouterX = linksFromRouterGpsLat
 		FromRouterY = linksFromRouterGpsLong
 		FromRouterZ = linksFromRouterGpsAlt
 		if debugFlag {
-			//			fmt.Println("router", Name, "GPS coordinates =", GpsLat, GpsLong, GpsAlt)
 			fmt.Println("returned from getRouterCoordinatesName func: FromRouterX=", FromRouterX, "FromRouterY=", FromRouterY, "FromRouterZ=", FromRouterZ)
 		}
 
@@ -298,7 +238,6 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 			fmt.Println("ToRouterName=", link.ToRouterName)
 			fmt.Println("ToRouterIP=", link.ToRouterIP)
 		}
-		//		ToRouterX, ToRouterY, ToRouterZ = getRouterCoordinatesName(debugFlag, routers, link.ToRouterName)
 		routerGpsRows, err = databaseForRead.Query("SELECT Name, GpsLat, GpsLong, GpsAlt FROM Routers WHERE Name = $1", ToRouterName)
 		if err != nil {
 			log.Fatalln("databaseForRead Query error", err.Error())
@@ -316,31 +255,16 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 		ToRouterZ = linksToRouterGpsAlt
 
 		if debugFlag {
-			//			fmt.Println("router coordinates =", routers[routerArrayIndex].System.G)
 			fmt.Println("router", Name, "GPS coordinates =", GpsLat, GpsLong, GpsAlt)
-			//			fmt.Println("returned from getRouterCoordinates func: ToRouterX=", ToRouterX, "ToRouterY=", ToRouterY, "ToRouterZ=", ToRouterZ)
 			fmt.Println("returned from getRouterCoordinatesIP func: ToRouterX=", ToRouterX, "ToRouterY=", ToRouterY, "ToRouterZ=", ToRouterZ)
 		}
 
 		// Add link object to the 3D scene
-		// <add gen code for line here>
-		//		link3D := geometry.NewCylinder(routerRadius, routerRadius, 0.5, 16, 2, 0, 2*math.Pi, true, true)
-		//		linkMat := material.NewPhong(math32.NewColor("Blue"))
-		//		cylinderMesh := graphic.NewMesh(link3D, linkMat)
-		//		cylinderMesh.SetPosition(FromRouterX, FromRouterY, FromRouterZ)
-
-		// calculate sphere coordinates of link endpoints from GPS coordinates
-		//		fromX, fromY, fromZ := calcCoordinates(strconv.FormatFloat(float64(FromRouterX), 'g', 1, 64), strconv.FormatFloat(float64(FromRouterY), 'g', 1, 64), strconv.FormatFloat(float64(FromRouterZ), 'g', 1, 64))
-		//		toX, toY, toZ := calcCoordinates(strconv.FormatFloat(float64(ToRouterX), 'g', 1, 64), strconv.FormatFloat(float64(ToRouterY), 'g', 1, 64), strconv.FormatFloat(float64(ToRouterZ), 'g', 1, 64))
 		fromX, fromY, fromZ := calcCoordinates(FromRouterX, FromRouterY, FromRouterZ)
 		toX, toY, toZ := calcCoordinates(ToRouterX, ToRouterY, ToRouterZ)
 
 		linkGeom := geometry.NewGeometry()
 		vertices := math32.NewArrayF32(0, 0)
-		//		vertices.Append(
-		//			FromRouterX, FromRouterY, FromRouterZ,
-		//			ToRouterX, ToRouterY, ToRouterZ,
-		//		)
 		vertices.Append(
 			fromX, fromY, fromZ,
 			toX, toY, toZ,
@@ -358,20 +282,12 @@ func visualizeNetwork(debugFlag bool, databaseForRead *sql.DB) *sql.DB {
 
 		linkGeom.AddVBO(gls.NewVBO(colors).AddAttrib(gls.VertexColor))
 
-		/*
-			linkFromVector := math32.NewVector3(FromRouterX, FromRouterY, FromRouterZ)
-			linkToVector := math32.NewVector3(ToRouterX, ToRouterY, ToRouterZ)
-			linkLine := math32.NewLine3(linkFromVector, linkToVector)
-			linkGeom.AddVBO(gls.NewVBO(linkLine))
-		*/
-
 		// Creates basic material
 		mat := material.NewBasic()
 
 		// Creates lines with the specified geometry and material
 		link3D := graphic.NewLines(linkGeom, mat)
 
-		//		app.Scene().Add(cylinderMesh)
 		app.Scene().Add(link3D)
 
 		err = linkRows.Err()
@@ -388,7 +304,6 @@ func calcCoordinates(GpsLat string, GpsLong string, GpsAlt string) (float32, flo
 	var x, y, z float32
 
 	var GpsLatFloat64 = 0.0
-	//		GpsLatFloat64, parseErr := strconv.ParseFloat(GpsLat, 64)
 	var parseErr error
 	if GpsLat != "" {
 		GpsLatFloat64, parseErr = strconv.ParseFloat(GpsLat, 64)
@@ -400,7 +315,6 @@ func calcCoordinates(GpsLat string, GpsLong string, GpsAlt string) (float32, flo
 	xRadianLat := Rad(GpsLatFloat64)
 
 	var GpsLongFloat64 = 0.0
-	//		GpsLongFloat64, parseErr := strconv.ParseFloat(GpsLong, 64)
 	if GpsLong != "" {
 		GpsLongFloat64, parseErr = strconv.ParseFloat(GpsLong, 64)
 		if parseErr != nil {
