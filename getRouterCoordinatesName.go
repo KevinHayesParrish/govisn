98d1a7f96a10566b6bf1827560171c619bef96d7
Kevin Parrish <kparrish@surewest.net>
2020-05-31 13:45:34 -0700

    Removed Commented lines.

--- getRouterCoordinatesName.go
+++ getRouterCoordinatesName.go
@@ -12,27 +12,19 @@ func getRouterCoordinatesName(debug bool, routers []Router, routerName string) (
 
 	//	for i := 0; i < len(routerArray); i++ {
 	for i := 0; i < len(routers); i++ {
-		//		if routerArray[i].System.Name == "" {
-		//			break // end of routerArray entries
-		//		}
-		//		if routerArray[i].System.Name == routerName {
 		if routers[i].System.Name == routerName {
-			//			x = routerArray[i].System.Coordinates.X
-			//			x1, err := strconv.ParseFloat(routerArray[i].System.GPS.Longitude, 32)
 			x1, err := strconv.ParseFloat(routers[i].System.GPS.Longitude, 32)
 			if err != nil {
 				panic(err)
 			}
 			x = (float32)(x1)
-			//			y = routerArray[i].System.Coordinates.Y
-			//			y1, err := strconv.ParseFloat(routerArray[i].System.GPS.Latitude, 32)
+
 			y1, err := strconv.ParseFloat(routers[i].System.GPS.Latitude, 32)
 			if err != nil {
 				panic(err)
 			}
 			y = (float32)(y1)
-			//			z = routerArray[i].System.Coordinates.Z
-			//			z1, err := strconv.ParseFloat(routerArray[i].System.GPS.Altitude, 32)
+
 			z1, err := strconv.ParseFloat(routers[i].System.GPS.Altitude, 32)
 			if err != nil {
 				panic(err)
