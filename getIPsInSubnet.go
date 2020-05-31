f366332b821dd7a22641402938b83fef34b77226
Kevin Parrish <kparrish@surewest.net>
2020-05-31 16:04:09 -0700

    Removed main func. Example was a standalone program. We will use it as a func within the govisn program.

--- getIPsInSubnet.go
+++ getIPsInSubnet.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net"
 )
 
@@ -16,15 +14,15 @@ func Hosts(cidr string) ([]string, error) {
 	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
 		ips = append(ips, ip.String())
 	}
-	
+
 	// remove network address and broadcast address
 	lenIPs := len(ips)
 	switch {
 	case lenIPs < 2:
 		return ips, nil
-	
+
 	default:
-	return ips[1 : len(ips)-1], nil
+		return ips[1 : len(ips)-1], nil
 	}
 }
 
@@ -37,6 +35,7 @@ func inc(ip net.IP) {
 	}
 }
 
+/*
 func main() {
 	tmp, err := Hosts("192.168.1.1/24")
 	if err != nil {
@@ -50,3 +49,4 @@ func main() {
 	}
 	fmt.Println(tmp)
 }
+*/
