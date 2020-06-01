7bb9bfbdba0084812c60112de161c2a9a970cde1
Kevin Parrish <kparrish@surewest.net>
2020-05-31 17:09:37 -0700

    Added code to scan a CIDR subnet given -s startup option input. Added code to SNMP Get the sysName and sysServices. Determines if host is a Router by examining sysServices. If it is, appends sysName and IP address to scannedRouters []string. An issue exists with FQDN lookup when no IP response.

--- scanNet.go
+++ scanNet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 	g "github.com/soniah/gosnmp"
@@ -15,7 +16,7 @@ import (
 // SCANNETVERSION is the file version number
 const SCANNETVERSION = "0.0.1"
 
-func scanNet(debugFlag bool, seed string, community string, params g.GoSNMP) []ScannedRouter {
+func scanNet(debugFlag bool, cidr string, community string, params g.GoSNMP) []ScannedRouter {
 
 	fmt.Println("\nfunc scanNet version", SCANNETVERSION, "started.")
 	if debugFlag {
@@ -23,6 +24,61 @@ func scanNet(debugFlag bool, seed string, community string, params g.GoSNMP) []S
 	}
 
 	var scannedRouters []ScannedRouter
+
+	/*
+	 * TODO:
+	 * Add scanning loop.
+	 * SNMP Get for each IP address
+	 * If Router System.Services is => IP services, then add Router FQDN and IP Address to scannedRouters
+
+	 */
+
+	// get all the addresses within the cidr subnet, given the input parameter.
+	subnetIPAddrs, err := getHosts(cidr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if debugFlag {
+		fmt.Println(len(subnetIPAddrs), "Host IP Addresses to be scanned=", subnetIPAddrs)
+	}
+
+	// Query all IP Addresses in the requested CIDR subnet
+	//	err = params.Connect()
+	//	if err != nil {
+	//		log.Fatalf("Connect() err: %v", err)
+	//	}
+	//	defer params.Conn.Close()
+
+	for i := 0; i < len(subnetIPAddrs); i++ {
+		// get sysName and sysServices
+		oids := []string{
+			sysNameOID + ".0",     // sysName
+			sysServicesOID + ".0", // sysServices
+		}
+
+		fqdn := getRtrName(subnetIPAddrs[i])
+		params.Target = subnetIPAddrs[i]
+		// Build SNMP connection to Router
+		err = params.Connect()
+		if err != nil {
+			//			log.Fatalf("Connect() err: %v", err)
+			fmt.Println("Router not SNMP Enabled, or SNMP parameters incorrect. Continuing to scann CIDR.")
+			params.Conn.Close()
+			continue
+		}
+
+		result, err := params.Get(oids) // Get() accepts up to g.MAX_OIDS
+		if err != nil {
+			log.Fatalf("Get() err: %v", err)
+		}
+		var scannedRouter ScannedRouter
+		if result.Variables[1].Value.(int) >= 4 {
+			scannedRouter.Name = fqdn[0]
+			scannedRouter.IPAddress = subnetIPAddrs[i]
+			scannedRouters = append(scannedRouters, scannedRouter)
+		}
+		params.Conn.Close()
+	}
 
 	if debugFlag {
 		fmt.Println("Returning, scannedRouters=", scannedRouters)
