464ab381c7b423ca3fa8de66c9a6914a68b4731f
Kevin Parrish <kparrish@surewest.net>
2020-05-31 17:10:17 -0700

    Modified default text for -scanNetFlag.

--- viewnet.go
+++ viewnet.go
@@ -46,7 +46,7 @@ var maxHops = flag.String("m", "0", "Scope of discovery. Maximum number of Hops 
 var visualizeFlag = flag.Bool("vi", false, "Visualize the Network.")
 
 //scanNetFlag is the startup option to scan the network for SNMP capable routers.
-var scanNetFlag = flag.String("s", "", "Scan the network for SNMP capable routers.\nOnce the network is scanned, the list of found routers\nwill be queried and their information added to the database.")
+var scanNetFlag = flag.String("s", "", "Scan the CIDR network for SNMP capable routers.\nCIDR format = x.x.x.x/n. ex: 192.168.1.0/24\nOnce the network is scanned, the list of found routers\nwill be queried and their information added to the database.")
 
 //routerRadius is the radius of the 3D object representing a network router
 const routerRadius float64 = 0.5
