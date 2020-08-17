745ce6e4310bcdec5f7817527c4a70d1c2ad0219
Kevin Parrish <kparrish@surewest.net>
2020-08-16 21:20:10 -0700

    initial version.

---
+++ walkRouteTable.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+
+	//"log"
+
+	"github.com/g3n/engine/util/logger"
+	_ "github.com/mattn/go-sqlite3"
+	g "github.com/soniah/gosnmp"
+)
+
+var scannedRouters []ScannedRouter
+
+// WALKROUTETABLEVERSION is the file version number
+const WALKROUTETABLEVERSION = "0.1.0"
+
+func walkRouteTable(log *logger.Logger, seed string, community string, params *g.GoSNMP) []ScannedRouter {
+
+	log.Info("\nfunc walkRouteTable version %s", WALKROUTETABLEVERSION+" started.")
+	log.Debug("seed= %s", seed+" community= "+community)
+
+	//	var scannedRouters []ScannedRouter
+
+	// get sysName and sysServices
+	//	oids := []string{
+	//		sysNameOID + ".0",     // sysName
+	//		sysServicesOID + ".0", // sysServices
+	//	}
+
+	fqdn := getRtrName(seed)
+	params.Target = seed
+
+	// Build SNMP connection to Router
+	err := params.Connect()
+	if err != nil {
+		log.Warn("Router not SNMP Enabled, or SNMP parameters incorrect. Continuing to scan CIDR.")
+		params.Conn.Close()
+	}
+
+	//	result, err := params.Get(oids) // Get() accepts up to g.MAX_OIDS
+	//	if err != nil {
+	//		if strings.Contains(err.Error(), "Request timeout") || strings.Contains(err.Error(), "connection refused") {
+	//			log.Warn(seed + " not answering SNMP get. Continuing network scan.")
+	//		} else {
+	//			log.Fatal("Get() err: %v", err)
+	//		}
+	//	}
+
+	var scannedRouter ScannedRouter
+
+	// Add seed router to list of routers
+	//	if result.Variables[1].Value.(int) >= 4 {
+	//		scannedRouter.Name = fqdn[0]
+	//		scannedRouter.IPAddress = seed
+	//		scannedRouters = append(scannedRouters, scannedRouter)
+	//	}
+	scannedRouter.IPAddress = seed
+	fqdn = getRtrName(scannedRouter.IPAddress)
+	scannedRouter.Name = fqdn[0]
+	scannedRouters = append(scannedRouters, scannedRouter)
+
+	// Retrieve the route table and add each Next Hop address to the list of routers
+	ipRouteNextHopPDU, err := params.WalkAll(ipRouteNextHopOID)
+	if err != nil {
+		log.Fatal("Get(ipRouteNextHopPDU) err")
+	}
+	log.Debug("\nipRouteNextHopPDU PDU= %v", ipRouteNextHopPDU)
+
+	for i := 0; i < len(ipRouteNextHopPDU); i++ {
+		scannedRouter.IPAddress = ipRouteNextHopPDU[i].Value.(string)
+		fqdn = getRtrName(scannedRouter.IPAddress)
+		scannedRouter.Name = fqdn[0]
+		scannedRouters = append(scannedRouters, scannedRouter)
+	}
+
+	nbrOfRouters := len(scannedRouters)
+	for j := 0; j < nbrOfRouters; j++ {
+		params.Target = scannedRouters[j].IPAddress
+		log.Debug("Recusively calling walkRouteTable." + scannedRouters[j].IPAddress + " params.Target=" + params.Target)
+		scannedRouters = walkRouteTable(log, scannedRouters[j].IPAddress, community, params)
+	}
+
+	params.Conn.Close()
+	log.Info("\nfunc walkRouteTable version %s ", WALKROUTETABLEVERSION+" ended.")
+	return scannedRouters
+}
