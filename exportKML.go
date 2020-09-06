6a0f8842b4619da9de18f67b28933632cb1c8489
Kevin Parrish <kparrish@surewest.net>
2020-09-06 11:42:19 -0700

    Added go-kml code to represent the routers. Next is to add the links between routers.

--- exportKML.go
+++ exportKML.go
@@ -25,25 +25,29 @@ func exportKML(log *logger.Logger, kmlFilename string, DbName string) {
 	defer databaseForRead.Close()
 
 	// Retrieve the Routers table
-	//	routerRows, queryErr := databaseForRead.Query("SELECT RouterID, Name, Description, UpTime, Contact, Location, Services, GpsLat, GpsLong, GpsAlt FROM Routers")
 	routerRows, queryErr := databaseForRead.Query("SELECT Name, Description, Location, GpsLat, GpsLong, GpsAlt FROM Routers")
 	if queryErr != nil {
 		log.Fatal("databaseForRead Query error %v", queryErr)
 	}
 	log.Debug("Successful Routers table Select")
 
-	//var routers []Router
-	//var router Router
 	var Name string
 	var Description string
 	var Location string
 	var GpsLat string
 	var GpsLong string
 	var GpsAlt string
-	//	routerArrayIndex := 0
 
-	//enc := xml.NewEncoder(os.Stdout)
-	//enc.Indent("  ", "    ")
+	k := kml.KML(
+		kml.Placemark(
+			kml.Name("GoVision"),
+			kml.Description("KML representation of a network."),
+		),
+	)
+	kmlFile, err := os.Create(kmlFilename)
+	if err != nil {
+		log.Fatal("Cannot Create %s", kmlFilename+" error="+err.Error())
+	}
 
 	var GpsLongFloat float64
 	var GpsLatFloat float64
@@ -52,29 +56,25 @@ func exportKML(log *logger.Logger, kmlFilename string, DbName string) {
 	for routerRows.Next() {
 		routerRows.Scan(&Name, &Description, &Location, &GpsLat, &GpsLong, &GpsAlt)
 
-		// Load router struct from DB fields
-		//router.System.Name = Name
-		//router.System.Location = Location
-		//router.System.GPS.Latitude = GpsLat
-		//router.System.GPS.Longitude = GpsLong
-		//router.System.GPS.Altitude = GpsAlt
 		s, _ := strconv.ParseFloat(GpsLong, 64)
 		GpsLongFloat = s
 		s, _ = strconv.ParseFloat(GpsLat, 64)
 		GpsLatFloat = s
 		s, _ = strconv.ParseFloat(GpsAlt, 64)
 		GpsAltFloat = s
-		k := kml.KML(
+
+		k.Add(
 			kml.Placemark(
 				kml.Name(Name),
 				kml.Description(Description),
-				kml.Coordinates(kml.Coordinate{Lon: GpsLongFloat, Lat: GpsLatFloat, Alt: GpsAltFloat}),
+				kml.Point(
+					kml.Coordinates(kml.Coordinate{Lon: GpsLongFloat, Lat: GpsLatFloat, Alt: GpsAltFloat}),
+				),
 			),
 		)
-
-		if err := k.WriteIndent(os.Stdout, "", " "); err != nil {
-			log.Fatal(err.Error())
-		}
+	}
+	if err := k.WriteIndent(kmlFile, "", "  "); err != nil {
+		log.Fatal(err.Error())
 	}
 
 	log.Info("exportKML version %s ", EXPORTKMLVERSION+" ending")
