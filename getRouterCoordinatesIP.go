89f886bf202e727b65eea527e4b374d57939e0ef
Kevin Parrish <kevin.parrish@mycci.net>
2026-02-13 14:36:47 -0800

    Commented unused functions.

--- getRouterCoordinatesIP.go
+++ getRouterCoordinatesIP.go
@@ -4,69 +4,73 @@
 
 package main
 
-import (
-	"database/sql"
-	"strconv"
-
-	"github.com/g3n/engine/util/logger"
-)
-
-/*
- * func getRouterCoordinatesIP retrieves Router Coordinates from the database
- * using the router's ToRouterIP.
- */
-func getRouterCoordinatesIP(database *sql.DB, ToRouterIPIn string) (float32, float32, float32) {
-	var log *logger.Logger
-	log.Debug("getRouterCoordiantesIP starting")
-
-	var x float32
-	var y float32
-	var z float32
-
-	LinksTableRows, err := database.Query("SELECT RouterID  FROM RouterIP WHERE IpAddr = $1", ToRouterIPIn)
-	if err != nil {
-		log.Fatal("LinksTableRows error")
-	}
-	log.Debug("Successful LinksTableRows Query")
-
-	defer LinksTableRows.Close()
-
-	var RouterIDLinks string
-	var RouterIDRouter, GpsLat, GpsLong, GpsAlt string
-	for LinksTableRows.Next() {
-		LinksTableRows.Scan(&RouterIDLinks)
-
-		RouterTableRows, err := database.Query("SELECT RouterID, GpsLat, GpsLong, GpsAlt FROM Routers WHERE RouterID = $1", RouterIDLinks)
-		if err != nil {
-			log.Fatal("RouterTableRows error: %v", err)
-		}
-		defer LinksTableRows.Close()
-
-		log.Debug("Successful RouterTable Query")
-
-		for RouterTableRows.Next() {
-			RouterTableRows.Scan(&RouterIDRouter, &GpsLat, &GpsLong, &GpsAlt)
-		}
-
-		x1, parseErr := strconv.ParseFloat(GpsLong, 32)
-		if parseErr != nil {
-			log.Fatal("x1 ParseFloat error: %v", parseErr)
-		}
-		x = (float32)(x1)
-
-		y1, parseErr := strconv.ParseFloat(GpsLat, 32)
-		if parseErr != nil {
-			log.Fatal("y1 ParseFloat error: %v", parseErr)
-		}
-		y = (float32)(y1)
-
-		z1, parseErr := strconv.ParseFloat(GpsAlt, 32)
-		if parseErr != nil {
-			log.Fatal("z1 ParseFloat error %v", parseErr)
-		}
-		z = (float32)(z1)
-	}
-	log.Debug("getRouterCoordiantesIP ending")
-
-	return x, y, z
-}
+/*--------------------------------------------------------------------------------------------------------------------------------------------*/
+/*                                                                                                                                            */
+/* import (                                                                                                                                   */
+/*     "database/sql"                                                                                                                         */
+/*     "strconv"                                                                                                                              */
+/*                                                                                                                                            */
+/*     "github.com/g3n/engine/util/logger"                                                                                                    */
+/* )                                                                                                                                          */
+/*                                                                                                                                            */
+/*                                                                                                                                            */
+/*  * func getRouterCoordinatesIP retrieves Router Coordinates from the database                                                              */
+/*  * using the router's ToRouterIP.                                                                                                          */
+/*                                                                                                                                            */
+/* func getRouterCoordinatesIP(database *sql.DB, ToRouterIPIn string) (float32, float32, float32) {                                           */
+/*     var log *logger.Logger                                                                                                                 */
+/*     log.Debug("getRouterCoordiantesIP starting")                                                                                           */
+/*                                                                                                                                            */
+/*     var x float32                                                                                                                          */
+/*     var y float32                                                                                                                          */
+/*     var z float32                                                                                                                          */
+/*                                                                                                                                            */
+/*     LinksTableRows, err := database.Query("SELECT RouterID  FROM RouterIP WHERE IpAddr = $1", ToRouterIPIn)                                */
+/*     if err != nil {                                                                                                                        */
+/*         log.Fatal("LinksTableRows error")                                                                                                  */
+/*     }                                                                                                                                      */
+/*     log.Debug("Successful LinksTableRows Query")                                                                                           */
+/*                                                                                                                                            */
+/*     defer LinksTableRows.Close()                                                                                                           */
+/*                                                                                                                                            */
+/*     var RouterIDLinks string                                                                                                               */
+/*     var RouterIDRouter, GpsLat, GpsLong, GpsAlt string                                                                                     */
+/*     for LinksTableRows.Next() {                                                                                                            */
+/*         LinksTableRows.Scan(&RouterIDLinks)                                                                                                */
+/*                                                                                                                                            */
+/*         RouterTableRows, err := database.Query("SELECT RouterID, GpsLat, GpsLong, GpsAlt FROM Routers WHERE RouterID = $1", RouterIDLinks) */
+/*         if err != nil {                                                                                                                    */
+/*             log.Fatal("RouterTableRows error: %v", err)                                                                                    */
+/*         }                                                                                                                                  */
+/*         defer LinksTableRows.Close()                                                                                                       */
+/*                                                                                                                                            */
+/*         log.Debug("Successful RouterTable Query")                                                                                          */
+/*                                                                                                                                            */
+/*         for RouterTableRows.Next() {                                                                                                       */
+/*             RouterTableRows.Scan(&RouterIDRouter, &GpsLat, &GpsLong, &GpsAlt)                                                              */
+/*         }                                                                                                                                  */
+/*                                                                                                                                            */
+/*         x1, parseErr := strconv.ParseFloat(GpsLong, 32)                                                                                    */
+/*         if parseErr != nil {                                                                                                               */
+/*             log.Fatal("x1 ParseFloat error: %v", parseErr)                                                                                 */
+/*         }                                                                                                                                  */
+/*         x = (float32)(x1)                                                                                                                  */
+/*                                                                                                                                            */
+/*         y1, parseErr := strconv.ParseFloat(GpsLat, 32)                                                                                     */
+/*         if parseErr != nil {                                                                                                               */
+/*             log.Fatal("y1 ParseFloat error: %v", parseErr)                                                                                 */
+/*         }                                                                                                                                  */
+/*         y = (float32)(y1)                                                                                                                  */
+/*                                                                                                                                            */
+/*         z1, parseErr := strconv.ParseFloat(GpsAlt, 32)                                                                                     */
+/*         if parseErr != nil {                                                                                                               */
+/*             log.Fatal("z1 ParseFloat error %v", parseErr)                                                                                  */
+/*         }                                                                                                                                  */
+/*         z = (float32)(z1)                                                                                                                  */
+/*     }                                                                                                                                      */
+/*     log.Debug("getRouterCoordiantesIP ending")                                                                                             */
+/*                                                                                                                                            */
+/*     return x, y, z                                                                                                                         */
+/* }                                                                                                                                          */
+/*                                                                                                                                            */
+/*--------------------------------------------------------------------------------------------------------------------------------------------*/
--- getRouterCoordinatesName.go
+++ getRouterCoordinatesName.go
@@ -4,43 +4,47 @@
 
 package main
 
-import (
-	"strconv"
-)
-
-/*
- * func getRouterCoordinatesName retrieves Router Coordinates from the database
- * using the router's name.
- */
-func getRouterCoordinatesName(routers []Router, routerName string) (float32, float32, float32) {
-	log.Debug("getRouterCoordinatesName starting")
-	var x float32
-	var y float32
-	var z float32
-
-	//	for i := 0; i < len(routerArray); i++ {
-	for i := 0; i < len(routers); i++ {
-		if routers[i].System.Name == routerName {
-			x1, err := strconv.ParseFloat(routers[i].System.GPS.Longitude, 32)
-			if err != nil {
-				panic(err)
-			}
-			x = (float32)(x1)
-
-			y1, err := strconv.ParseFloat(routers[i].System.GPS.Latitude, 32)
-			if err != nil {
-				panic(err)
-			}
-			y = (float32)(y1)
-
-			z1, err := strconv.ParseFloat(routers[i].System.GPS.Altitude, 32)
-			if err != nil {
-				panic(err)
-			}
-			z = (float32)(z1)
-			break
-		}
-	}
-	log.Debug("getRouterCoordinatesName ending")
-	return x, y, z
-}
+/*--------------------------------------------------------------------------------------------------*/
+/*                                                                                                  */
+/* import (                                                                                         */
+/*     "strconv"                                                                                    */
+/* )                                                                                                */
+/*                                                                                                  */
+/*                                                                                                  */
+/*  * func getRouterCoordinatesName retrieves Router Coordinates from the database                  */
+/*  * using the router's name.                                                                      */
+/*                                                                                                  */
+/* func getRouterCoordinatesName(routers []Router, routerName string) (float32, float32, float32) { */
+/*     log.Debug("getRouterCoordinatesName starting")                                               */
+/*     var x float32                                                                                */
+/*     var y float32                                                                                */
+/*     var z float32                                                                                */
+/*                                                                                                  */
+/*           for i := 0; i < len(routerArray); i++ {                                                */
+/*     for i := 0; i < len(routers); i++ {                                                          */
+/*         if routers[i].System.Name == routerName {                                                */
+/*             x1, err := strconv.ParseFloat(routers[i].System.GPS.Longitude, 32)                   */
+/*             if err != nil {                                                                      */
+/*                 panic(err)                                                                       */
+/*             }                                                                                    */
+/*             x = (float32)(x1)                                                                    */
+/*                                                                                                  */
+/*             y1, err := strconv.ParseFloat(routers[i].System.GPS.Latitude, 32)                    */
+/*             if err != nil {                                                                      */
+/*                 panic(err)                                                                       */
+/*             }                                                                                    */
+/*             y = (float32)(y1)                                                                    */
+/*                                                                                                  */
+/*             z1, err := strconv.ParseFloat(routers[i].System.GPS.Altitude, 32)                    */
+/*             if err != nil {                                                                      */
+/*                 panic(err)                                                                       */
+/*             }                                                                                    */
+/*             z = (float32)(z1)                                                                    */
+/*             break                                                                                */
+/*         }                                                                                        */
+/*     }                                                                                            */
+/*     log.Debug("getRouterCoordinatesName ending")                                                 */
+/*     return x, y, z                                                                               */
+/* }                                                                                                */
+/*                                                                                                  */
+/*--------------------------------------------------------------------------------------------------*/
